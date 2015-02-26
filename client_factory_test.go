7dda1a149080e097f44c22c1d73f7e6edf6f869a
Khash Sajadi <khash@cloud66.com>
2015-02-26 17:01:27 +0000

    relax response payload type

--- client_factory_test.go
+++ client_factory_test.go
@@ -3,42 +3,42 @@ package wray
 import "time"
 
 type FakeSchedular struct {
-  requestedDelay time.Duration
+	requestedDelay time.Duration
 }
 
-func(self *FakeSchedular) wait(delay time.Duration, callback func()) {
-  self.requestedDelay = delay
+func (self *FakeSchedular) wait(delay time.Duration, callback func()) {
+	self.requestedDelay = delay
 }
 
-func(self *FakeSchedular) delay() time.Duration {
-  return self.requestedDelay
+func (self *FakeSchedular) delay() time.Duration {
+	return self.requestedDelay
 }
 
 type FayeClientBuilder struct {
-  client FayeClient
+	client FayeClient
 }
 
 func BuildFayeClient() FayeClientBuilder {
-  schedular := &FakeSchedular{}
-  client := FayeClient{state: UNCONNECTED, url: "https://localhost", clientId: "client1", schedular: schedular}
-  return FayeClientBuilder{client}
+	schedular := &FakeSchedular{}
+	client := FayeClient{state: UNCONNECTED, url: "https://localhost", clientId: "client1", schedular: schedular}
+	return FayeClientBuilder{client}
 }
 
-func(self FayeClientBuilder) Client() FayeClient {
-  return self.client
+func (self FayeClientBuilder) Client() FayeClient {
+	return self.client
 }
 
-func(self FayeClientBuilder) Connected() FayeClientBuilder {
-  self.client.state = CONNECTED
-  return self
+func (self FayeClientBuilder) Connected() FayeClientBuilder {
+	self.client.state = CONNECTED
+	return self
 }
 
-func(self FayeClientBuilder) WithTransport(transport Transport) FayeClientBuilder {
-  self.client.transport = transport
-  return self
+func (self FayeClientBuilder) WithTransport(transport Transport) FayeClientBuilder {
+	self.client.transport = transport
+	return self
 }
 
-func(self FayeClientBuilder) WithSubscriptions(subscriptions []Subscription) FayeClientBuilder {
-  self.client.subscriptions = subscriptions
-  return self
+func (self FayeClientBuilder) WithSubscriptions(subscriptions []Subscription) FayeClientBuilder {
+	self.client.subscriptions = subscriptions
+	return self
 }
